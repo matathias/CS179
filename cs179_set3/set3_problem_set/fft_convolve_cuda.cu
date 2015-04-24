0da19f0ae71fa9e520fecb3238abe05db50ee2b8
matathias <warrick.david58@gmail.com>
2015-04-24 03:30:43 -0700

    Test...

--- cs179_set3/set3_problem_set/fft_convolve_cuda.cu
+++ cs179_set3/set3_problem_set/fft_convolve_cuda.cu
@@ -109,6 +109,7 @@ cudaMaximumKernel(cufftComplex *out_data, float *max_abs_val,
     for (int j = 0; j < numFloats && index + j < padded_length; j++) {
         // We want the absolute value of out_data, not the complex value.
         float real = abs(out_data[index + j].x);
+        real = numFloats; //~test~
         if (j == 0) {
             data[threadIdx.x] = real;
         }
@@ -124,10 +125,8 @@ cudaMaximumKernel(cufftComplex *out_data, float *max_abs_val,
     // that each thread will handle only numFloats values. This will be the case
     // in every iteration of the loop - every thread will only compare 
     // numFloats values.
-    int strideLength = 2;// blockDim.x / 2;
-    while (strideLength * 2 < blockDim.x) {
-        strideLength = strideLength * 2;
-    }
+    int strideLength = blockDim.x / 2;
+    
     while (strideLength >= 1) {
         int ind = threadIdx.x;
         // If the thread index is less than the stride length, then continue.
@@ -135,7 +134,7 @@ cudaMaximumKernel(cufftComplex *out_data, float *max_abs_val,
         // of values that are compared is reduced by a factor of two, and as
         // each thread compares numFloats values, the number of threads that are
         // utilized is also reduced by a factor of two.
-        if (ind < strideLength && ind + strideLength < padded_length) {
+        if (ind < strideLength) {
             // Compare numFloats values, each one stride length apart. The max
             // value of these values will end up assigned to data[ind].
             /*for (int i = 0; i < 2; i++) {
