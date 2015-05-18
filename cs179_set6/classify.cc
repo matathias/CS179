050cc8facf59a4beb4c720711cfa9ed640c88e7c
matathias <warrick.david58@gmail.com>
2015-05-17 18:44:54 -0700

    Minor changes

--- cs179_set6/classify.cc
+++ cs179_set6/classify.cc
@@ -100,7 +100,7 @@ void classify(istream& in_stream, int batch_size) {
   float *d_weights;
   gpuErrChk(cudaMalloc(&d_weights, sizeof(float) * REVIEW_DIM));
   
-  int step_size = 0.1;
+  int step_size = 1;
   float classification_time = -1;
 
   // main loop to process input lines (each line corresponds to a review)
