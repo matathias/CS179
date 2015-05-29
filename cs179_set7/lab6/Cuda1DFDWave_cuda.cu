34af49160929c6ad8ec332e6fcb1ae935df09baa
matathias <warrick.david58@gmail.com>
2015-05-28 20:51:26 -0700

    Removed an if statment in the kernal

--- cs179_set7/lab6/Cuda1DFDWave_cuda.cu
+++ cs179_set7/lab6/Cuda1DFDWave_cuda.cu
@@ -18,23 +18,21 @@ void waveEquationKernal(float *old_data, float *current_data, float *new_data,
                         int numberOfNodes, float constant) {
     
     unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
-    if (index == 0)
-        index += blockDim.x * gridDim.x;
+    // This is to make sure that thread index 0 can still move on to the
+    // next thread at blockDim.x * gridDim.x
+    //if (index == 0)
+    //    index += blockDim.x * gridDim.x;
         
     while (index > 0 && index < numberOfNodes - 1) {
-        // This is to make sure that thread index 0 can still move on to the
-        // next thread at blockDim.x * gridDim.x
-        //if (index > 0) {
-            // Wave Equation!
-            // y_x,t+1 = 2*y_x,t - y_x,t-1 + 
-            //                       (c*dt/dx)^2 * (y_x+1,t - 2*y_x,t + y_x-1,t)
-            new_data[index] = 2 * current_data[index] 
-                              - old_data[index]
-                              + constant 
-                              * (current_data[index + 1] 
-                                 - 2 * current_data[index] 
-                                 + current_data[index - 1]);
-        //}
+        // Wave Equation!
+        // y_x,t+1 = 2*y_x,t - y_x,t-1 + 
+        //                       (c*dt/dx)^2 * (y_x+1,t - 2*y_x,t + y_x-1,t)
+        new_data[index] = 2 * current_data[index] 
+                          - old_data[index]
+                          + constant 
+                          * (current_data[index + 1] 
+                             - 2 * current_data[index] 
+                             + current_data[index - 1]);
         index += blockDim.x * gridDim.x;
     }
 }
