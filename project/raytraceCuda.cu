5fa805a510d6da373e16f4656a0f6840d552f138
matathias <warrick.david58@gmail.com>
2015-06-02 00:43:52 -0700

    Weird pointer issues...

--- project/raytraceCuda.cu
+++ project/raytraceCuda.cu
@@ -159,7 +159,8 @@ double isq(double *vec, double e, double n)
     printf("&vec[0]: %p vec[0]: %f\n", &vec[0], vec[0]);
     printf("&vec[1]: %p vec[1]: %f\n", &vec[1], vec[1]);
     printf("&vec[2]: %p vec[2]: %f\n", &vec[2], vec[2]);
-    double xTerm = pow(pow(vec[0], 2.0), 1.0 / (double) e);
+    double tmpVal = pow(vec[0], 2.0);
+    double xTerm = pow(tmpVal, 1.0 / (double) e);
     double yTerm = pow(pow(vec[1], 2.0), 1.0 / (double) e);
     double xyTerm = pow(xTerm + yTerm, e / (double) n);
     return xyTerm + zTerm - 1.0;
